bcba43405b5d5505e5bff6ecd44e57a22ba6dded
KTimurka <91308426+KTimurka@users.noreply.github.com>
2021-09-28 18:21:35 +0300

    Объекты записаны как функции

--- lab3/2_drawex.py
+++ lab3/2_drawex.py
@@ -6,6 +6,8 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((800,600))
 
+
+
 #Цвета:
 black = (0,0,0)
 white = (255,255,255)
@@ -16,30 +18,36 @@ yellow = (255,255,0)
 darkblue = (50,50,255)
 
 
+
 #Рисуем дерево
 tree = [6, green, 50, 600, 200]
-dx = 60
-dy = 30
-number, color, rad, x,y = tree
-polygon(screen,brown,[(x+10,y+90),(x+10,y+300),(x-10,y+300),(x-10,y+90)])
-circle(screen, color, (x,y),rad)
-circle(screen, (0,0,0), (x,y),rad,2)
-for i in range(number):
-        circle(screen, color, (x+dx/2,y+dy),rad)
-        circle(screen, (0,0,0), (x+dx/2,y+dy),rad,2)
-        x = x + dx*(-1)**(i+1)
-        y = y + dy*(1+(-1)**(i+1))
+def derevo(tree,dx,dy):
+    number, color, rad, x,y = tree
+    polygon(screen,brown,[(x+10,y+90),(x+10,y+300),(x-10,y+300),(x-10,y+90)])
+    circle(screen, color, (x,y),rad)
+    circle(screen, (0,0,0), (x,y),rad,2)
+    for i in range(number):
+            circle(screen, color, (x+dx/2,y+dy),rad)
+            circle(screen, (0,0,0), (x+dx/2,y+dy),rad,2)
+            x = x + dx*(-1)**(i+1)
+            y = y + dy*(1+(-1)**(i+1))
+derevo(tree,60,30)
 
+        
 #Рисуем облако
 cloud = [7,white ,50,100,100]
-dx1 = 30
-dy1 = 60
-number1, color1, rad1,x1,y1 = cloud
-for k in range(number1):
-        circle(screen, color1, (x1,y1),rad1)
-        circle(screen, (0,0,0), (x1,y1),rad1,2)
-        y1 = y1 + dy1*((-1)**k)
-        x1 = x1 + dx1*(1+(-1)**(k+1))
+def oblaka(cloud,dx,dy):
+    number, color, rad,x,y = cloud
+    for k in range(number):
+            circle(screen, color, (x,y),rad)
+            circle(screen, (0,0,0), (x,y),rad,2)
+            y = y + dy*((-1)**k)
+            x = x + dx*(1+(-1)**(k+1))
+oblaka(cloud,30,60)
+
+#Рисуем дом
+
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
