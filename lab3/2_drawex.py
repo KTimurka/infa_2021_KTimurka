e958ddfb051164cd066fef7e7740ef29b05022a3
KTimurka <91308426+KTimurka@users.noreply.github.com>
2021-09-28 18:13:01 +0300

    Цвета как отдельные переменные

--- lab3/2_drawex.py
+++ lab3/2_drawex.py
@@ -6,12 +6,22 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((800,600))
 
+#Цвета:
+black = (0,0,0)
+white = (255,255,255)
+green = (0,255,0)
+blue = (0,0,255)
+brown = (100,100,100)
+yellow = (255,255,0)
+darkblue = (50,50,255)
+
+
 #Рисуем дерево
-tree = [6, (0,255,0), 50, 600,200]
+tree = [6, green, 50, 600, 200]
 dx = 60
 dy = 30
 number, color, rad, x,y = tree
-polygon(screen,(100,100,100),[(x+10,y+90),(x+10,y+300),(x-10,y+300),(x-10,y+90)])
+polygon(screen,brown,[(x+10,y+90),(x+10,y+300),(x-10,y+300),(x-10,y+90)])
 circle(screen, color, (x,y),rad)
 circle(screen, (0,0,0), (x,y),rad,2)
 for i in range(number):
@@ -21,7 +31,7 @@ for i in range(number):
         y = y + dy*(1+(-1)**(i+1))
 
 #Рисуем облако
-cloud = [7,(255,255,255),50,100,100]
+cloud = [7,white ,50,100,100]
 dx1 = 30
 dy1 = 60
 number1, color1, rad1,x1,y1 = cloud
@@ -35,10 +45,10 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-while finished == False:
+while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            finished == True
+            finished = True
 
 pygame.quit()
